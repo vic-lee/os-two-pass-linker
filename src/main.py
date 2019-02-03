d381449b6baa498107d26e89a9e3cb01ff821a69
vic-lee <viclee@nyu.edu>
2019-02-03 16:13:26 -0500

    Refactor process component code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,3 @@
-'''
-A module contains 1) definition list 2) use list 3) program text
-'''
-
 import os, sys, re
 
 def get_input():
@@ -16,22 +12,7 @@ def get_input():
     user_input = " ".join(user_input).split()
     return user_input
 
-def process_mod_count(s):
-    pass
-
 def parse_mod(user_input): 
-    '''
-    Return a list of modules, parsed from user input. 
-    Raise errors if there are less modules than mod_count. 
-    - get mod_count
-    - for mod_count times, repeat: 
-        - get the first number after mod_count as def_count
-        - repeat def pair storing process for def_count times
-        - get the next number as use_count
-        - repeat use pair storing process for use_count times
-        - get the next number as prog_count
-        - repeat prog pair storing process for prog_count times 
-    '''
     print("mod count is " + user_input[0])
     MOD_COUNT = "mod_count"
     MODS = "mods"
@@ -39,11 +20,42 @@ def parse_mod(user_input):
         MOD_COUNT: int(user_input[0]), 
         MODS: []
     }
-    pending_ls = user_input[1:]
-    for _ in range(progs["mod_count"]): 
-        mod, pending_ls = mod_ls_to_dict(pending_ls)
-        progs["mods"].append(mod)
+    pending_list = user_input[1:]
+    for _ in range(progs[MOD_COUNT]): 
+        mod, pending_list = mod_ls_to_dict(pending_list)
+        progs[MODS].append(mod)
     return progs
+
+def process_mod_component(component, mod, cur): 
+
+    COUNT = "count"
+    LIST = "list"
+    SYM = "sym"
+    ADDR = "rel_addr"
+
+    COUNT = component + '_' + COUNT
+    LIST = component + '_' + LIST
+    
+    comp = {
+        COUNT: None,
+        LIST: []
+    }
+
+    comp[COUNT] = int(mod[cur])
+    cur += 1
+    
+    while cur < len(mod):
+        if comp[COUNT] is not None and len(comp[LIST]) < comp[COUNT]: 
+            comp[LIST].append({
+                SYM: mod[cur],
+                ADDR: mod[cur + 1]
+            })
+            cur += 2
+            continue
+        else: 
+            break
+
+    return comp, cur
 
 def mod_ls_to_dict(md_in): 
     '''
@@ -52,71 +64,22 @@ def mod_ls_to_dict(md_in):
     '''
 
     DEF = "def"
-    DEF_COUNT = "def_count"
-    DEF_LIST = "def_list"
     USE = "use"
-    USE_COUNT = "use_count"
-    USE_LIST = "use_list"
     PROG = "prog"
-    PROG_COUNT = "prog_count"
-    PROG_LIST = "prog_list"
-    SYM = "sym"
-    ADDR = "rel_addr"
 
     md_out = {
-        DEF: {
-            DEF_COUNT: None,
-            DEF_LIST: []
-        }, 
-        USE: {
-            USE_COUNT: None,
-            USE_LIST: []
-        }, 
-        PROG: {
-            PROG_COUNT: None, 
-            PROG_LIST: []
-        }
+        DEF: {}, 
+        USE: {}, 
+        PROG: {}
     }
+
     cur = 0
-    while cur < len(md_in):
-        if md_out[DEF][DEF_COUNT] is None: 
-            md_out[DEF][DEF_COUNT] = int(md_in[cur])
-            cur += 1
-            continue 
-        elif md_out[DEF][DEF_COUNT] is not None and len(md_out[DEF][DEF_LIST]) < md_out[DEF][DEF_COUNT]: 
-            md_out[DEF][DEF_LIST].append({
-                SYM: md_in[cur],
-                ADDR: md_in[cur + 1]
-            })
-            cur += 2
-            continue
-        elif md_out[USE][USE_COUNT] is None:
-            md_out[USE][USE_COUNT] = int(md_in[cur])
-            cur += 1
-            continue 
-        elif md_out[USE][USE_COUNT] is not None and len(md_out[USE][USE_LIST]) < md_out[USE][USE_COUNT]:
-            md_out[USE][USE_LIST].append({
-                SYM: md_in[cur],
-                ADDR: md_in[cur + 1]
-            })
-            cur += 2
-            continue
-        elif md_out[PROG][PROG_COUNT] is None:
-            md_out[PROG][PROG_COUNT] = int(md_in[cur])
-            cur += 1
-            continue
-        elif md_out[PROG][PROG_COUNT] is not None and len(md_out[PROG][PROG_LIST]) < md_out[PROG][PROG_COUNT]: 
-            md_out[PROG][PROG_LIST].append({
-                SYM: md_in[cur],
-                ADDR: md_in[cur + 1]
-            })
-            cur += 2
-            continue
-        else: 
-            break
-    # print(md_out)
+
+    md_out[DEF], cur = process_mod_component(DEF, md_in, cur)
+    md_out[USE], cur = process_mod_component(USE, md_in, cur)
+    md_out[PROG], cur = process_mod_component(PROG, md_in, cur)
+
     return (md_out, md_in[cur:])
-    
 
 def main():
     u_in = get_input()
