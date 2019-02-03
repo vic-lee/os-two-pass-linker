7c56636f7652f239249b14156302d8132a4478ee
vic-lee <viclee@nyu.edu>
2019-02-03 13:47:28 -0500

    Add comment for parse module algorithm

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+'''
+A module contains 1) definition list 2) use list 3) program text
+'''
+
 import os, sys, re
 
 def get_input():
@@ -17,6 +21,21 @@ def get_mod_count(s):
         if c.isdigit():
             return c    
     return None
+
+def parse_mod(user_input, mod_count): 
+    '''
+    Return a list of modules, parsed from user input. 
+    Raise errors if there are less modules than mod_count. 
+    - for mod_count times, repeat: 
+        - get the first number after mod_count as def_count
+        - repeat def pair storing process for def_count times
+        - get the next number as use_count
+        - repeat use pair storing process for use_count times
+        - get the next number as prog_count
+        - repeat prog pair storing process for prog_count times 
+    '''
+    mods = []
+    pass
 
 def main():
     input_set = get_input()
