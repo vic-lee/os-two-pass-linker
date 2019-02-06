320d25b7611760717a625adccf4a37a2153ea234
vic-lee <viclee@nyu.edu>
2019-02-06 10:32:39 -0500

    Finish resolving external addresses

--- src/main.py
+++ src/main.py
@@ -4,8 +4,15 @@ from collections import ChainMap
 DEF = "def"
 USE = "use"
 PROG = "prog"
+BASE = "base"
 MOD_COUNT = "mod_count"
 MODS = "mods"
+TYPE = "type"
+WORD = "word"
+
+def print_list(l):
+    for item in l: 
+        print(item)
 
 def get_input():
     print("Paste your input, then enter Ctrl-D to exit input mode:")
@@ -31,9 +38,7 @@ def process_user_input(u_in):
     return progs
 
 def process_mod_component(component, mod, cur, base=0): 
-    TYPE = "type"
-    WORD = "word"
-    BASE = "base"
+
     COUNT = component + '_count'
     LIST = component + '_list'
     comp = {}
@@ -78,29 +83,74 @@ def parse_mod(md_in, base):
 
 def generate_sym_table(mods):
     sym_table = {}
-    base_accum = 0
     for mod in mods[MODS]:
         def_dict = mod[DEF]["def_list"]
         for sym in def_dict:
-            def_dict[sym] += base_accum
+            def_dict[sym] += mod[PROG][BASE]
         sym_table.update(def_dict)
-        base_accum += mod[PROG]["prog_count"]
     return sym_table
 
-def resolve_addresses(mods):
-    for mod in mods[MODS]: 
-        prog = mod[PROG]
-        print(prog)
-        print("\n")
+def process_external_addr(old_addr, new_addr):
+    first_digit = int(str(old_addr)[0])
+    return (first_digit * 1000 + new_addr)
+
+def resolve_external_addr(prog_list, addr, new_addr):
     pass
+
+def resolve_addresses(mods, sym_table): 
+    for mod in mods[MODS]:
+        use_list = mod[USE]['use_list']
+        prog = mod[PROG]
+        prog_list = prog['prog_list']
+        for usym, uaddr in use_list.items():
+            '''Resolve external addresses'''
+            old_sym_addr = prog_list[uaddr][WORD]
+            addr_cur = str(old_sym_addr)
+            new_sym_addr = sym_table[usym]
+            prog_list[uaddr][WORD] = process_external_addr(old_sym_addr, new_sym_addr)
+            while addr_cur[-3:] != '777':
+                next_addr = str(prog_list[int(addr_cur[-3:])][WORD])
+                prog_list[int(addr_cur[-3:])][WORD] = process_external_addr(int(next_addr), new_sym_addr)
+                addr_cur = next_addr
+        for progpair in prog_list: 
+            '''Resolve relative addresses'''
+            if progpair[TYPE] == 'R': 
+                progpair[WORD] += prog[BASE]
+        print_list(prog_list)
+        print('\n')
+    return mods
+
+# def resolve_addresses(mods, sym_table):
+#     for mod in mods[MODS]: 
+#         used = mod[USE]["use_list"]
+#         prog = mod[PROG]
+#         prog_list = prog["prog_list"]
+#         for used_sym, used_addr in used.items():
+#             '''TODO: 
+#             1) handle if this addr is not E; 
+#             2) handle if list is shorter than used_addr
+#             3) Handle if sym table does not contain this used sym
+#             '''
+#             print(used_sym)
+#             old_addr = prog_list[used_addr][WORD]
+#             new_addr = sym_table[used_sym]
+#             prog_list = resolve_external_addr(prog_list, old_addr, new_addr)
+#         for pair in prog_list: 
+#             if pair[TYPE] == "R":
+#                 pair[WORD] += prog[BASE]
+#         print(prog)
+#         print('\n')
+#     return mods
 
 def main():
     user_input = get_input()
+    print('\n')
     processed_mod = process_user_input(user_input)
     # print("progs is " + str(processed_mod))
     sym_table = generate_sym_table(processed_mod)
-    # print(sym_table)
-    resolve_addresses(processed_mod)
+    print(sym_table)
+    print('\n')
+    processed_mod = resolve_addresses(processed_mod, sym_table)
     
 
 if __name__ == "__main__":
